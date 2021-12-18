0d494249695adcf826f3d513467c6e24bdf41dda
Shanti-kumari9 <shantik20@navgurukul.org>
2021-12-18 20:09:56 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+
+function myFunction() {
+    var first=document.getElementById('first_num').value
+    
+    var second=document.getElementById('second_num').value
+    
+
+    document.getElementById("out").innerHTML=parseInt(first)+parseInt(second)
+}
+
+
+
+function Subtract() {
+    var first=document.getElementById('first_num').value
+    
+    var second=document.getElementById('second_num').value
+
+    document.getElementById("outt").innerHTML=parseInt(first)-parseInt(second)
+}
+
+
+function Multiplacation() {
+    var first=document.getElementById('first_num').value
+    
+    var second=document.getElementById('second_num').value
+
+    document.getElementById("outtt").innerHTML=parseInt(first)*parseInt(second)
+}
+
+
+
+function Division() {
+    var first=document.getElementById('first_num').value
+    
+    var second=document.getElementById('second_num').value
+    document.getElementById("outttt").innerHTML=parseInt(first)/parseInt(second)
+}
+
+
